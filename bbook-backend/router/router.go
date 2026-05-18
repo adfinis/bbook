122240edf21a2d55cfc38b54eec8effe3d63fbd2
Albert Cerfeda <albert.cerfeda@adfinis.com>
2026-05-18 16:29:47 +0200

    refactor: consolidate router module

--- bbook-backend/router/router.go
+++ bbook-backend/router/router.go
@@ -1,12 +1,25 @@
 package router
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 
+	"git.adfinis.com/albertc/bbook/bbook-backend/database"
+	"git.adfinis.com/albertc/bbook/bbook-backend/server/search"
 	"git.adfinis.com/albertc/bbook/bbook-backend/static"
+	"git.adfinis.com/albertc/bbook/bbook-backend/templates"
 	"github.com/gorilla/mux"
 )
+
+// Contains all htmx templates
+var tmpl = template.Must(template.ParseFS(templates.FS, "*.html"))
+
+type indexTmplData struct {
+	Query      string
+	Contacts   []database.ContactView
+	FieldNames []string
+}
 
 func Router() *mux.Router {
 	r := mux.NewRouter()
@@ -17,7 +30,23 @@ func Router() *mux.Router {
 		w.Write([]byte("pong!"))
 	})
 
-	r.Methods("GET").Path("/").HandlerFunc(handleIndex)
+	r.Methods("GET").Path("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		q := r.URL.Query().Get("q")
+
+		contacts, err := search.Search(q)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if err := tmpl.ExecuteTemplate(w, "index.html", indexTmplData{
+			Query:      q,
+			Contacts:   contacts,
+			FieldNames: search.FieldNames,
+		}); err != nil {
+			log.Printf("rendering index: %v", err)
+		}
+	})
 
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.FS(static.FS))))
 
