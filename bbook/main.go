ff10c6e09d9b83c2f4a71525060ff671eafe9df0
Albert Cerfeda <albert.cerfeda@adfinis.com>
2026-07-08 11:30:57 +0200

    chore: remove test

--- bbook/main.go
+++ bbook/main.go
@@ -28,41 +28,6 @@ func main() {
 		log.Fatalf("auth: %v", err)
 	}
 
-	// Test
-	// ctx := context.Background()
-	// contacts, err := zoho.FetchContacts(ctx)
-	// if err != nil {
-	// 	log.Fatalf("zoho: %v", err)
-	// }
-	// syncedAt := time.Now()
-	// for _, c := range contacts {
-	// 	c.SyncedAt = syncedAt
-	// 	if err := database.Client.Queries.UpsertContact(ctx, database.UpsertContactParams(c)); err != nil {
-	// 		log.Fatalf("upsert: %v", err)
-	// 	}
-	// }
-	// log.Printf("fetched %d contacts", len(contacts))
-	// stored, err := database.Client.Queries.AllContacts(ctx)
-	// if err != nil {
-	// 	log.Fatalf("read back: %v", err)
-	// }
-	// log.Printf("stored %d contacts", len(stored))
-
-	// ratelimit, err := zoho.FetchRateLimit(ctx)
-	// if err != nil {
-	// 	log.Fatalf("zoho: %v", err)
-	// }
-	// fmt.Println(ratelimit.Limit, ratelimit.Remaining, ratelimit.ResetAt)
-	//
-	//
-	// contacts, err := zoho.FetchContacts(context.Background())
-	// if err != nil {
-	// 	log.Fatalf("zoho: %v", err)
-	// }
-	// log.Println("Success", len(contacts), "contacts fetched")
-	// b, _ := json.MarshalIndent(contacts[0], "", "  ")
-	// fmt.Println(string(b))
-
 	cmd := &cli.Command{
 		Name:                  "bbook",
 		Usage:                 "BBook backend",
